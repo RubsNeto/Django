d9d0dc6cc8ca3575ef291c4e3cf6c313121583dd
Rubens Neto <50962890+RubsNeto@users.noreply.github.com>
2023-11-13 20:20:33 -0300

    dia 13

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = "index"),
+    path('<int:id>', views.index, name = "index"),
+    path('', views.v1, name = "view1"),
     path('v1/', views.v1, name = "view1"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import toDoList, Item
 # Create your views here.
 
 
-def index(response):
-    return HttpResponse("<h1>Page 1</h1>")
+def index(response, id):
+    ls = toDoList.objects.get(id = id)
+    return HttpResponse("<h1>%s</h1>" %ls.name)
 
 def v1(response):
-    return HttpResponse("<h1>view 1!</h1>")
+    return HttpResponse("<h1>Screen 1!</h1>")
